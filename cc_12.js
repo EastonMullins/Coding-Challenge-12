b30fd709002debd9ec8708c8a932f1b638fd8b9e
Easton <eastonmullins@usf.edu>
2025-03-02 18:34:03 -0500

    Task 2 - Updated Metric Cards Via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -12,3 +12,14 @@ revenueCard.innerHTML = "<h3>Revenue</h3><p>$0</p>";
 
 dashboard1.appendChild(revenueCard);
 
+
+// Task 2 - Updated Metric Cards Via Array Conversion
+
+const metricCards = document.querySelectorAll(".metric-card");
+
+const metricCardsArray = Array.from(metricCards);
+
+metricCardsArray.forEach(card => {
+    card.innerHTML += " - Updated";
+    card.style.backgroundColor = "cyan"
+});
