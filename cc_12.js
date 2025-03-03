6a3fd0b3cbe4bb3d8ff714e5fe87e86474c41ad4
Easton <eastonmullins@usf.edu>
2025-03-02 19:15:13 -0500

    Task 4 - Demonstrated Event Bubbling in Customer Section

--- cc_12.js
+++ cc_12.js
@@ -4,13 +4,25 @@ const dashboard1 = document.getElementById("dashboard");
 const dashboard2 = document.querySelector("#dashboard");
 
 const revenueCard = document.createElement(`div`);
+const profitCard = document.createElement("div");
+const expensesCard = document.createElement("div");
 
 revenueCard.setAttribute("class", "metric-card");
 revenueCard.setAttribute("id", "revenueCard");
 
+profitCard.setAttribute("class","metric-card");
+profitCard.setAttribute("id", "profitCard");
+
+expensesCard.setAttribute("class","metric-card");
+expensesCard.setAttribute("id", "expensesCard");
+
 revenueCard.innerHTML = "<h3>Revenue</h3><p>$0</p>";
+profitCard.innerHTML = "<h3>Profit</h3><p>$0</p>"
+expensesCard.innerHTML = "<h3>Expenses</h3><p>$0</p>"
 
 dashboard1.appendChild(revenueCard);
+dashboard1.appendChild(profitCard);
+dashboard1.appendChild(expensesCard);
 
 
 // Task 2 - Updated Metric Cards Via Array Conversion
@@ -37,3 +49,21 @@ function addInventoryItem(productName) {
     li.innerHTML = `${productName}`;
     inventoryList.appendChild(li);
 }
+
+// Task 4 - Demonstrated Event Bubbling in Customer Section
+
+const customerSection = document.getElementById("customerSection");
+
+customerSection.addEventListener("click", () => {
+    console.log("Customer Section Clicked");
+});
+
+const customerCards = document.querySelectorAll(".customer-card");
+
+customerCards.forEach(card => {
+    card.addEventListener("click", (event) => {
+        console.log("Customer Card Clicked");
+
+        event.stopPropagation();
+    });
+});
